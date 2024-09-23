ad98073b8aef4d5e9e4dec19cb6e880002f8a062
Javoxir Toshpo'latov <javohirtoshpulatov1303@gmail.com>
2024-09-23 09:37:12 +0500

    Add project files.

---
+++ Shell_In_Csharp/Program.cs
@@ -0,0 +1,97 @@
+﻿using Npgsql;
+using Shell_In_Csharp;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.Write("Enter Host: ");
+        string? host = Console.ReadLine();
+        Console.Write("Enter port: ");
+        string? port = Console.ReadLine();
+        Console.Write("Enter Database: ");
+        string? database = Console.ReadLine();
+        Console.Write("Enter Usename: ");
+        string? username = Console.ReadLine();
+        Console.Write("Enter password: ");
+        string? password = Console.ReadLine();
+        var connection = SchemaMenu.ConnectToDatabase(host, port, database, username, password);
+
+        using (var conn = connection)
+        {
+            Console.WriteLine("Successfully connected to database");
+            Console.ReadLine();
+            ArrowIndex(new List<string> { "Schemas" }, " ");
+            //int key = ArrowIndex(ShowSchemas(conn), "Schemas");
+
+            schema:
+            List<string> list1 = new List<string>()
+            {
+                "\t\t1.Functions",
+                "\t\t2.Procedures",
+                "\t\t3.Tables",
+                "\t\t4.Views",
+                "\t\t5.Sequences",
+                "\t\t6.Back"
+
+            };
+
+            int index = ArrowIndex(list1, " ");
+
+            switch (index)
+            {
+                case 0:
+                    SchemaMenu.RetrievePostgresFunctions(conn);
+                    Console.ReadLine();
+                    goto schema;
+                    break;
+                case 1:
+                    SchemaMenu.RetrievePostgresFunctions(conn);
+                    Console.ReadLine();
+                    goto schema;
+                    break;
+                case 2:
+                    SchemaMenu.DatabaseTables(conn);
+                    Console.ReadLine();
+                    goto schema;
+                    break;
+                case 3:
+                    SchemaMenu.RetrievePostgresViews(conn);
+                    Console.ReadLine();
+                    goto schema;
+                    break;
+                case 4:
+                    break;
+            }
+        }
+    }
+
+    
+    
+    public static int ArrowIndex(List<string> list, string name)
+    {
+        int selectIndex = 0;
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t\t" + name);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i == selectIndex)
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                Console.WriteLine(list[i]);
+                Console.ResetColor();
+            }
+
+            ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+            if (consoleKeyInfo.Key == ConsoleKey.UpArrow) selectIndex = (selectIndex - 1 + list.Count) % list.Count;
+            else if (consoleKeyInfo.Key == ConsoleKey.DownArrow) selectIndex = (selectIndex + 1) % list.Count;
+            else if (consoleKeyInfo.Key == ConsoleKey.Enter) return selectIndex;
+        }
+    }
+}
---
+++ Shell_In_Csharp/SchemaMenu.cs
@@ -0,0 +1,134 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace Shell_In_Csharp
+{
+    public static class SchemaMenu
+    {
+        public static void DatabaseTables(NpgsqlConnection con)
+        {
+            using (var conn = con)
+            {
+                conn.Open();
+                var Schemas = con.GetSchema("Tables");
+
+                foreach (DataRow schema in Schemas.Rows)
+                {
+                    Console.WriteLine(schema["TABLE_NAME"]);
+                }
+                con.Close();
+            }
+        }
+
+        public static NpgsqlConnection ConnectToDatabase(string host, string port, string database, string Username, string password)
+        {
+            string connectionString = $"Host={host}; port={port};Database={database}; User Id={Username}; Password={password}";
+            NpgsqlConnection con = new NpgsqlConnection(connectionString);
+            return con;
+        }
+
+        public static List<string> ShowSchemas(NpgsqlConnection conn)
+        {
+            conn.Open();
+
+            using (var cmd = new NpgsqlCommand("SELECT nspname AS schema_name FROM pg_namespace WHERE nspname NOT IN ('information_schema', 'pg_catalog', 'pg_toast') ORDER BY nspname;", conn))
+            {
+                using (var reader = cmd.ExecuteReader())
+                {
+                    int t = 1;
+                    List<string> list = new List<string>();
+                    while (reader.Read())
+                    {
+                        var schema = $"{t}.{reader.GetString(0)}";  // Print schema name
+                        list.Add(schema);
+                        t++;
+                    }
+                    list.Add($"{t}.Back");
+                    return list;
+                }
+            }
+        }
+
+        public static void RetrievePostgresFunctions(NpgsqlConnection con)
+        {
+            // SQL query to retrieve function names in the 'public' schema
+            string query = @"
+                SELECT 
+                    p.proname AS function_name
+                    FROM pg_catalog.pg_proc p
+                    LEFT JOIN pg_catalog.pg_namespace n ON n.oid = p.pronamespace
+                    WHERE n.nspname = 'public'
+                    ORDER BY function_name;
+                ";
+
+            using (var cmd = new NpgsqlCommand(query, con))
+            {
+                con.Open();
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Function: {reader["function_name"]}");
+                    }
+                }
+                con.Close();
+            }
+        }
+
+        public static void RetrievePostgresProcedures(NpgsqlConnection con)
+        {
+            // SQL query to retrieve procedure names in the 'public' schema
+            string query = @"
+                SELECT 
+                    p.proname AS procedure_name
+                    FROM pg_catalog.pg_proc p
+                    LEFT JOIN pg_catalog.pg_namespace n ON n.oid = p.pronamespace
+                    WHERE n.nspname = 'public'
+                    AND p.prokind = 'p'  -- 'p' indicates stored procedures
+                    ORDER BY procedure_name;
+                ";
+
+            using (var cmd = new NpgsqlCommand(query, con))
+            {
+                con.Open();
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Procedure: {reader["procedure_name"]}");
+                    }
+                }
+                con.Close();
+            }
+        }
+
+
+        public static void RetrievePostgresViews(NpgsqlConnection con)
+        {
+            // SQL query to retrieve view names in the 'public' schema
+            string query = @"
+                SELECT 
+                    table_name AS view_name
+                    FROM information_schema.views
+                    WHERE table_schema = 'public'
+                    ORDER BY view_name;
+                ";
+
+            using (var cmd = new NpgsqlCommand(query, con))
+            {
+                con.Open();
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"View: {reader["view_name"]}");
+                    }
+                }
+                con.Close();
+            }
+        }
+
+
+
+    }
+}
